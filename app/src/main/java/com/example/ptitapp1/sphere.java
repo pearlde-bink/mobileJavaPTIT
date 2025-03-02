a94749b90e1308ccdc7f0801afc7d2339d4aa67b
Bink <tuanbinhnguyenngoc@gmail.com>
2025-03-02 14:49:37 +0700

    update after baithuchanh1

--- app/src/main/java/com/example/ptitapp1/sphere.java
+++ app/src/main/java/com/example/ptitapp1/sphere.java
@@ -14,10 +14,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class sphere extends AppCompatActivity {
-    private EditText r;
-    private RadioButton area, volume;
+    private EditText r, h;
     private Button btn;
-    private TextView kq;
+    private TextView kqa, kqv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,25 +24,25 @@ public class sphere extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_sphere);
 
-        r = findViewById(R.id.rad);
-        area = findViewById(R.id.area);
-        volume = findViewById(R.id.volume);
+        r = findViewById(R.id.radVal);
+        h = findViewById(R.id.heightVal);
         btn = findViewById(R.id.btn);
-        kq = findViewById(R.id.kq);
+        kqa = findViewById(R.id.kqa);
+        kqv = findViewById(R.id.kqv);
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String rad = r.getText().toString();
+                String height = h.getText().toString();
                 try {
                     double r = Double.parseDouble(rad);
-                    if(area.isChecked()){
-                        String kk = dientich(r);
-                        kq.setText(kk);
-                    } else if(volume.isChecked()){
-                        String kk = tt(r);
-                        kq.setText(kk);
-                    }
+                    double h = Double.parseDouble(height);
+                    String kk1 = area(r, h);
+                    kqa.setText(kk1);
+
+                    String kk2 = volume(r, h);
+                    kqv.setText(kk2);
                 } catch (NumberFormatException e){
                     System.out.println(e);
                 }
@@ -57,13 +56,39 @@ public class sphere extends AppCompatActivity {
         });
     }
 
-    private String dientich(double r){
-        double a = 4*3.14*r*r;
-        return a +"";
-    }
+    private String area(double radius, double height) {
+        // Using the provided value of π
+//        double pi = 3.14159265;
+//
+//        // Calculate the slant height using Pythagorean theorem: √(r² + h²)
+//        double slantHeight = Math.sqrt(radius * radius + height * height);
+//
+//        // Calculate surface area: πr² + πr√(r² + h²)
+//        double surfaceArea = (Math.PI * radius * radius) + Math.PI * radius * slantHeight;
+//
+//        return "Surface area = " + surfaceArea;
+        // Chuyển đổi sang kiểu số
+//        double r = Double.parseDouble(radius);
+//        double h = Double.parseDouble(height);
 
-    private String tt(double r){
-        double v = 4/3*3.14*r*r*r;
-        return v +"";
+        // Tính toán
+        double volume = (1.0 / 3.0) * Math.PI * radius * radius * height;  // Thể tích hình nón
+        double slantHeight = Math.sqrt(radius * radius + height * height);      // Độ dài đường sinh
+        double surfaceArea = Math.PI * radius * (radius + slantHeight); // Diện tích xung quanh
+
+        // Hiển thị kết quả
+//        resultVolume.setText(String.format("%.5f", volume));
+//        resultSurfaceArea.setText(String.format("%.5f", surfaceArea));
+        String s = String.format("%.5f", surfaceArea);
+        return "Area = " + s;
+    }
+    private String volume(double radius, double height) {
+        // Using the provided value of π
+        double pi = 3.14159265;
+
+        // Calculate volume using formula: (1/3)πr²h
+        double volume = (1.0/3.0) * Math.PI * radius * radius * height;
+        String s= String.format("%.5f", volume);
+        return "Volume = " + s;
     }
 }
